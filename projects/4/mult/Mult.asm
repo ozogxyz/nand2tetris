b9c60da278167f65bba4c6098024bc1415b11b0d
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2024-09-12 21:54:41 +0300

    Project 4, Multiplication in Assembly

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -8,3 +8,62 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+	// Before going into the loop check for zero values
+	@mult
+	M=0
+
+	@R0
+	D=M
+	@OUTPUT
+	D			; JEQ
+
+	@R1
+	D=M
+	@OUTPUT
+	D			; JEQ
+
+	// i = 0
+	@i
+	M=1
+
+	// if R0 or R1 are not zero then mult = R0
+	@R0
+	D=M
+	@mult
+	M=D
+
+	(LOOP)
+	// if i > R1 goto STOP
+	@i
+	D=M
+	@R1
+	D=D-M
+	@OUTPUT
+	D			;JGE
+
+	// mult = mult + R0
+	@mult
+	D=M
+	@R0
+	D=D+M
+	@mult
+	M=D
+
+	// n = n + 1
+	@i
+	M=M+1
+
+	@LOOP
+	0			;JMP
+
+	(OUTPUT)
+	// R2 = mult
+	@mult
+	D=M
+	@R2
+	M=D
+
+	(END)
+	@END
+	0			;JMP

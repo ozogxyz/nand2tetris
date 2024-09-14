618213669b03b2b653ba25e0f1ace39b9142b4be
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2024-09-14 08:34:47 +0300

    Project 4, Fill the screen when key is pressed and clear when no key in Assembly

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -3,9 +3,50 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/4/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, 
+// i.e. writes "black" in every pixel. When no key is pressed,
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+        @SCREEN
+	D=A
+	@addr
+	M=D
+	@KBD
+	D=M
+	@CLEAR
+	D; JEQ
+(FILL_INNER)
+	@KBD
+	D=A
+	@addr
+	D=D-M
+	@LOOP
+	D; JEQ
+	@addr
+	A=M
+	M=-1
+	@addr
+	M=M+1
+	@FILL_INNER
+	0; JMP
+(CLEAR)
+        @SCREEN
+	D=A
+	@addr
+	M=D
+(CLEAR_INNER)
+	@KBD
+	D=A
+	@addr
+	D=D-M
+	@LOOP
+	D; JEQ
+	@addr
+	A=M
+	M=0
+	@addr
+	M=M+1
+	@CLEAR_INNER
+	0; JMP
